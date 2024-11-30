1069ec9a1e9af4c17ed2c2cb531822bef8146466
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2024-11-29 18:23:10 -0600

    matchers

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,3 +1,5 @@
+import { getStringInfo } from "../app/utils";
+
 describe('Utils', () => {
   it('should return the uppercase text', () => {
     const text = 'Hello, world!';
@@ -7,4 +9,28 @@ describe('Utils', () => {
 
     expect(result).toBe(expected);
   });
+
+  //only se usa para correr solo un test
+  it.only('should return info for valid string', ()=>{
+    const actual = getStringInfo('My-String');
+  
+    expect(actual.lowerCase).toBe('my-string');
+
+    //toEqual compara los valores no las referencias
+    expect(actual.extraInfo).toEqual({})
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+    expect(actual.characters).toEqual(['M', 'y', '-','S', 't', 'r','i', 'n', 'g']);
+    expect(actual.characters).toContain<string>('M');
+
+    //expect.arrayContaining sirve para comparar arrays
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(['S', 't', 'r','i', 'n', 'g', 'M', 'y', '-'])
+    );
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
+});
 })
